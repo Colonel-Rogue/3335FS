486bab2beb41943a143d475df5e5ec48be37cd9a
TJ Challstrom <colonelrouge@gmail.com>
2016-09-22 19:31:30 -0500

    Delete Functionality and GUI additions -Added File List -Added Delete functionality (soft delete) -Added Delete file button -Renamed FileParsingUtilites -> FilesystemUtilities

--- src/com/challstrom/fs/FATFS.java
+++ src/com/challstrom/fs/FATFS.java
@@ -4,7 +4,10 @@
 
 package com.challstrom.fs;
 
+import com.sun.istack.internal.NotNull;
+
 import java.util.Arrays;
+import java.util.Objects;
 
 /**
  * Created by tchallst on 9/14/2016.
@@ -17,7 +20,7 @@ class FATFS {
     private FileInfo[] FileInfos;
     private int[] BlockInfo;
     private String[] Blocks;
-    private int BlocksAvailable = BLOCK_CAPACITY;
+    private int BlocksAvailable;
 
     FATFS(int ALLOCATION_UNIT_SIZE, int BLOCK_CAPACITY) {
         this.ALLOCATION_UNIT_SIZE = ALLOCATION_UNIT_SIZE;
@@ -25,12 +28,14 @@ class FATFS {
         FileInfos = new FileInfo[BLOCK_CAPACITY];
         BlockInfo = new int[BLOCK_CAPACITY];
         Blocks = new String[BLOCK_CAPACITY];
+        BlocksAvailable = BLOCK_CAPACITY;
     }
 
     boolean write(String filename, String inputString) {
-        String[] inputBlocks = FileParsingUtilities.covertToBlocks(ALLOCATION_UNIT_SIZE, inputString);
+        String[] inputBlocks = FilesystemUtilities.covertToBlocks(ALLOCATION_UNIT_SIZE, inputString);
         int next = getNextAvailableBlock(-1);
         int firstBlock = next;
+        int last = -1;
         for (String inputBlock : inputBlocks) {
             if (next < 0) {
                 System.err.println("ERROR! No Blocks Available! Please use a smaller file, or increase the Block Capacity!");
@@ -38,10 +43,11 @@ class FATFS {
             }
             Blocks[next] = inputBlock;
             BlockInfo[next] = getNextAvailableBlock(next);
+            last = next;
             next = getNextAvailableBlock(next);
             BlocksAvailable--;
         }
-        BlockInfo[next] = -1;
+        BlockInfo[last] = -1;
         FileInfo fileInfo = new FileInfo(filename, firstBlock);
         FileInfos[firstBlock] = fileInfo;
         return true;
@@ -66,6 +72,25 @@ class FATFS {
             i++;
         } while (blocks[i] != null);
         return output;
+    }
+
+    boolean drop(String filename) {
+        int fileInfoIndex = FilesystemUtilities.fseek(filename, FileInfos);
+        if (fileInfoIndex < 0) {
+            System.err.println("No file '" + filename + "' found!");
+            return false;
+        }
+        boolean dropped = freeBlock(FileInfos[fileInfoIndex].getBlockStart());
+        FileInfos[fileInfoIndex] = null;
+        return dropped;
+    }
+
+    private boolean freeBlock(int blockNumber) {
+        if (blockNumber < 0) return true;
+        int next = BlockInfo[blockNumber];
+        BlockInfo[blockNumber] = 0;
+        BlocksAvailable++;
+        return freeBlock(next);
     }
 
     private int getNextAvailableBlock(int last) {
@@ -118,6 +143,10 @@ class FATFS {
         return BLOCK_CAPACITY;
     }
 
+    public int getBlocksUsed() {
+        return ALLOCATION_UNIT_SIZE - BlocksAvailable;
+    }
+
     @Override
     public String toString() {
         return "FATFS{" + "\n\t" + "FileInfos=" + Arrays.toString(FileInfos) + "" +
@@ -159,9 +188,17 @@ class FileInfo {
     public int hashCode() {
         return filename.hashCode();
     }
+
+    public int getBlockStart() {
+        return blockStart;
+    }
+
+    String getFilename() {
+        return filename;
+    }
 }
 
-class FileParsingUtilities {
+class FilesystemUtilities {
     static String[] covertToBlocks(int allocation_unit_size, String inputString) {
         int size = (inputString.length() / allocation_unit_size) + (inputString.length() % allocation_unit_size > 0 ? 1 : 0);
         String[] output = new String[size];
@@ -170,6 +207,15 @@ class FileParsingUtilities {
         }
         //if(!isValidBlockStructure(output)) throw new RuntimeException("Block Structure Invalid!");
         return output;
+    }
+
+    @NotNull
+    static int fseek(String filename, FileInfo[] fileInfos) {
+        for (int i = 0; i < fileInfos.length; i++) {
+            if (fileInfos[i] == null) continue;
+            if (Objects.equals(fileInfos[i].getFilename(), filename)) return i;
+        }
+        return -1;
     }
 
 /*    static boolean isValidBlockStructure((int allocation_unit_size, String[] inputBlocks) {
--- src/com/challstrom/fs/MainController.java
+++ src/com/challstrom/fs/MainController.java
@@ -4,6 +4,8 @@
 
 package com.challstrom.fs;
 
+import com.sun.istack.internal.NotNull;
+
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
@@ -26,13 +28,15 @@ public class MainController {
 class MainGUI {
     private FATFS fatfs;
     private String buffer;
+    private String bufferPath;
 
     //GUI Construction
     private JFrame frame;
-    private JPanel blockAreaPanel;
     private JTextArea[] blockAreas;
+    private JComboBox<String> FileInfoArea;
 
     void showMainGUI() {
+        //Setup and init for first window
         JFrame.setDefaultLookAndFeelDecorated(true);
         JDialog.setDefaultLookAndFeelDecorated(true);
         frame = new JFrame("FATFS");
@@ -44,36 +48,45 @@ class MainGUI {
         JLabel allocationLabel = new JLabel("Allocation Unit Size: ");
         JLabel capacityLabel = new JLabel("Block Capacity: ");
 
+        //Add to frame
         frame.add(allocationLabel);
         frame.add(allocationField);
         frame.add(capacityLabel);
         frame.add(capacityField);
         frame.add(FATFSButton);
+
         frame.pack();
 
         FATFSButton.addActionListener(e -> showFATFS(Integer.parseInt(allocationField.getText()), Integer.parseInt(capacityField.getText())));
 
+        //final line
         frame.setVisible(true);
 
     }
 
     private void showFATFS(int allocationUnitSize, int blockCapacity) {
+        //Clear previous frame
         frame.getContentPane().removeAll();
         System.out.println(allocationUnitSize);
         System.out.println(blockCapacity);
         fatfs = new FATFS(allocationUnitSize, blockCapacity);
 
-        //Construction
+        //Setup and init
         JButton openFile = new JButton("Read File into buffer");
         JButton writeBuffer = new JButton("Write Buffer to FATFS");
+        JButton deleteFile = new JButton("Delete Selected File");
         blockAreas = new JTextArea[blockCapacity];
-        blockAreaPanel = new JPanel(new GridLayout(16, 16, 1, 1));
+        JPanel blockAreaPanel = new JPanel(new GridLayout(24, 12, 2, 2));
+        FileInfoArea = new JComboBox<>();
 
         //Add to components/frames
         frame.getContentPane().setLayout(new FlowLayout());
         frame.getContentPane().add(openFile);
         frame.getContentPane().add(writeBuffer);
+        frame.getContentPane().add(FileInfoArea);
+        frame.getContentPane().add(deleteFile);
         frame.getContentPane().add(blockAreaPanel);
+
         //Create and add block areas
         for (int i = 0; i < blockCapacity; i++) {
             blockAreas[i] = new JTextArea(2, allocationUnitSize / 50);
@@ -81,14 +94,18 @@ class MainGUI {
             blockAreaPanel.add(new JLabel("Block " + i));
             blockAreaPanel.add(blockAreas[i]);
         }
+
+        //Resize
         Dimension frameDimension = new Dimension();
-        frameDimension.setSize(frame.getContentPane().getPreferredSize().getWidth() + 10, frame.getContentPane().getPreferredSize().getHeight() + (100 * (blockCapacity / 256)));
+        frameDimension.setSize(frame.getContentPane().getPreferredSize().getWidth(), frame.getContentPane().getPreferredSize().getHeight() + (100 * (blockCapacity / 256)));
         frame.setSize(frameDimension);
         frame.revalidate();
 
         //Listeners
         openFile.addActionListener(e -> openFile());
         writeBuffer.addActionListener(e -> writeBufferToFATFS());
+        FileInfoArea.addActionListener(e -> deleteFile.setEnabled(true));
+        deleteFile.addActionListener(e -> deleteFile());
 
         //final line
         frame.setVisible(true);
@@ -102,6 +119,7 @@ class MainGUI {
             file = fileChooser.getSelectedFile();
         }
         buffer = ControllerUtilities.readFile(file);
+        bufferPath = file != null ? file.getName() : null;
     }
 
     private void writeBufferToFATFS() {
@@ -111,25 +129,49 @@ class MainGUI {
         if (Objects.equals(buffer, "")) {
             System.err.println("Buffer is empty!");
         }
-        fatfs.write("t1.txt", buffer);
+        fatfs.write(bufferPath, buffer);
         updateFrame();
     }
+
+    private void deleteFile() {
+        if (fatfs == null) {
+            throw new RuntimeException("FATFS has not been initialized!");
+        }
+        if (Objects.equals(FileInfoArea.getSelectedItem(), "")) {
+            System.err.println("No File selected for deletion!");
+        } else {
+            fatfs.drop((String) FileInfoArea.getSelectedItem());
+        }
+        updateFrame();
+    }
+
 
     private void updateFrame() {
         if (fatfs == null) {
             throw new RuntimeException("FATFS has not been initialized!");
         }
-        int blockCapacity = fatfs.getBLOCK_CAPACITY();
-        int allocationUnitSize = fatfs.getALLOCATION_UNIT_SIZE();
+        int blocksUsed = fatfs.getBlocksUsed();
         String[] blocks = fatfs.getBlocks();
-        for (int i = 0; i < blockCapacity; i++) {
-            blockAreas[i].setText("..." + blocks[i].substring(blocks[i].length() - 10, blocks[i].length() - 1));
+        FileInfo[] filesInfos = fatfs.getFileInfos();
+        for (int i = 0; i < blocksUsed; i++) {
+            if (blocks[i].length() > 10) {
+                if (blocks[i] == null) continue;
+                blockAreas[i].setText("..." + blocks[i].substring(blocks[i].length() - 10, blocks[i].length() - 1));
+            } else blockAreas[i].setText(blocks[i]);
+        }
+        FileInfoArea.removeAllItems();
+        for (FileInfo info :
+                filesInfos) {
+            if (info == null) continue;
+            FileInfoArea.addItem(info.getFilename());
         }
         frame.revalidate();
     }
 }
 
 class ControllerUtilities {
+
+    @NotNull
     static String readFile(File file) {
         String line = null;
         String out = "";
